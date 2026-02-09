c1c3de6dbf57fa892d1e837e3f1b248f6feccef0
Omar Mahmood <36893410+Omar717@users.noreply.github.com>
2026-02-09 18:27:55 +0000

    Add button
    Adds button allowing for grid size changes

--- javascript.js
+++ javascript.js
@@ -1,6 +1,12 @@
 const container = document.querySelector("#container");
+const button = document.querySelector("#button");
 
-function gridColors (container) {
+const gridBtn = document.createElement("button");
+gridBtn.textContent = "Change grid size!"
+gridBtn.classList.toggle("gridBtn");
+button.appendChild(gridBtn);
+
+function grid (container) {
     for (i = 0; i < 16; i++) {
         for (j = 0; j < 16; j++) {
             const squares = document.createElement("div");
@@ -9,11 +15,51 @@ function gridColors (container) {
         }
     }
 
-    const hover = document.querySelectorAll(".squares");
-    hover.forEach((square) => 
+    const allSquares = document.querySelectorAll(".squares");
+    allSquares.forEach((square) => 
         square.addEventListener("mouseenter", () => 
             square.style.backgroundColor = "black"));
+
+    Sizing = String((Math.sqrt((464*464)/(16*16))-2)+ "px");
+    allSquares.forEach((square) => square.style.height = Sizing);
+    allSquares.forEach((square) => square.style.width = Sizing);
     
 }
 
-gridColors(container);
+function changeGrid (size) {
+
+    numSize = Number(size);
+    
+    const severalSquares = document.querySelectorAll(".squares");
+    severalSquares.forEach((square) => container.removeChild(square));
+
+    if (numSize <= 100 && numSize > 0) {
+        for (i = 0; i < numSize; i++) {
+            for (j = 0; j < numSize; j++) {
+                const squares = document.createElement("div");
+                squares.classList.toggle("squares")
+                container.appendChild(squares);
+            }
+        }  
+    }
+    else {
+        alert("Error: enter a number between 1 and 100");
+        grid(container);
+    }
+
+    const allSquares = document.querySelectorAll(".squares");
+    allSquares.forEach((square) => 
+        square.addEventListener("mouseenter", () => 
+            square.style.backgroundColor = "black"));
+    
+    if (numSize <= 100 && numSize > 0) {    
+        Sizing = String((Math.sqrt((464*464)/(numSize*numSize))-2)+ "px");
+        allSquares.forEach((square) => square.style.height = Sizing);
+        allSquares.forEach((square) => square.style.width = Sizing);
+    }
+}
+
+
+gridBtn.addEventListener("click", () => changeGrid(prompt("Enter a number between 1 and 100")))
+
+grid(container);
